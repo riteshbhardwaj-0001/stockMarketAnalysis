3ef3ba827c6e08e2cd6d56c22b8cd4f577ecefc6
unknown <riteshbhardwaj00001@gmail.com>
2024-11-23 19:07:02 +0530

    Exchange and Candle Entity Created

---
+++ StockMarketAnalysis/src/main/java/com/StockMarket/StockMarketAnalysis/bean/Exchange.java
@@ -0,0 +1,56 @@
+package com.StockMarket.StockMarketAnalysis.bean;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="exchange_info")
+public class Exchange {
+
+    @Id
+    private Long id;
+    private String name;
+
+    @OneToOne(mappedBy = "exchange", cascade = CascadeType.ALL)
+    private Candle candle;
+
+    public Exchange(){
+
+    }
+
+    public Exchange(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Candle getCandle() {
+        return candle;
+    }
+
+    public void setCandle(Candle candle) {
+        this.candle = candle;
+    }
+
+    @Override
+    public String toString() {
+        return "Exchange{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
