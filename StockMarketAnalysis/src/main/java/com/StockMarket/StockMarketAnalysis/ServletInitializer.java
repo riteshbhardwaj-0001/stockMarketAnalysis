e46137633aaaa9cee9c5b1d0daa97fdaaca09cee
unknown <riteshbhardwaj00001@gmail.com>
2024-11-23 17:45:14 +0530

    Initial Commit

---
+++ StockMarketAnalysis/src/main/java/com/StockMarket/StockMarketAnalysis/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.StockMarket.StockMarketAnalysis;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StockMarketAnalysisApplication.class);
+	}
+
+}
---
+++ StockMarketAnalysis/src/test/java/com/StockMarket/StockMarketAnalysis/StockMarketAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockMarket.StockMarketAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
